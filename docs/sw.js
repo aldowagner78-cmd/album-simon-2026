2c18c831b27a83d1c634263893b53644a92c192b
aldowagner78-cmd <aldowagner78@gmail.com>
2026-05-09 20:54:35 -0300

    Auditoria: chips tamaño fijo + badge superindice repetidas + fix refreshHeader + remove codigo muerto

--- docs/sw.js
+++ docs/sw.js
@@ -1,5 +1,5 @@
 // Bump esta version cada vez que se publiquen cambios clave para forzar refresco en el celular.
-const CACHE_NAME = "album-simon-pages-v11";
+const CACHE_NAME = "album-simon-pages-v12";
 const ASSETS = [
   "./",
   "./index.html",
