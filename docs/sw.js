7128f8dc01d45a2ec6b96840037b6ff9db390233
aldowagner78-cmd <aldowagner78@gmail.com>
2026-05-09 20:07:55 -0300

    Secciones visibles: Las que tengo / Me faltan con thumbs y banderas

--- docs/sw.js
+++ docs/sw.js
@@ -1,5 +1,5 @@
 // Bump esta version cada vez que se publiquen cambios clave para forzar refresco en el celular.
-const CACHE_NAME = "album-simon-pages-v6";
+const CACHE_NAME = "album-simon-pages-v7";
 const ASSETS = [
   "./",
   "./index.html",
