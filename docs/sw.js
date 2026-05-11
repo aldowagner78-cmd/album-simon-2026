9ff4ea840e6177fe576c23b48b38bd45e3131573
aldowagner78-cmd <aldowagner78@gmail.com>
2026-05-11 16:15:48 -0300

    Data: KSA|6 Jihad Thakri - complete all 141 players

--- docs/sw.js
+++ docs/sw.js
@@ -1,5 +1,5 @@
 // Bump esta version cada vez que se publiquen cambios clave para forzar refresco en el celular.
-const CACHE_NAME = "album-simon-pages-v23";
+const CACHE_NAME = "album-simon-pages-v24";
 const ASSETS = [
   "./",
   "./index.html",
