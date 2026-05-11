de027e86b806bacd88bd687d89e713b71aabeb97
aldowagner78-cmd <aldowagner78@gmail.com>
2026-05-11 14:49:08 -0300

    fix: escudo muestra imagen limpia, equipo muestra foto limpia, solo jugadores tienen blur

--- docs/sw.js
+++ docs/sw.js
@@ -1,5 +1,5 @@
 // Bump esta version cada vez que se publiquen cambios clave para forzar refresco en el celular.
-const CACHE_NAME = "album-simon-pages-v17";
+const CACHE_NAME = "album-simon-pages-v18";
 const ASSETS = [
   "./",
   "./index.html",
