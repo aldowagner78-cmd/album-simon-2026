a2d074284ae106a686e282de0839af1ead0a7eb6
aldowagner78-cmd <aldowagner78@gmail.com>
2026-05-11 15:48:01 -0300

    fix: sticker#1 muestra bandera limpia, sin escudo mal recortado

--- docs/sw.js
+++ docs/sw.js
@@ -1,5 +1,5 @@
 // Bump esta version cada vez que se publiquen cambios clave para forzar refresco en el celular.
-const CACHE_NAME = "album-simon-pages-v19";
+const CACHE_NAME = "album-simon-pages-v20";
 const ASSETS = [
   "./",
   "./index.html",
