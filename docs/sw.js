6865a28f6dd1c6530559aa55829b0db50a071091
aldowagner78-cmd <aldowagner78@gmail.com>
2026-05-12 08:26:08 -0300

    feat(ui): igualar tamaño y orden de botones en resumen

--- docs/sw.js
+++ docs/sw.js
@@ -1,5 +1,5 @@
 // Bump esta version cada vez que se publiquen cambios clave para forzar refresco en el celular.
-const CACHE_NAME = "album-simon-pages-v32";
+const CACHE_NAME = "album-simon-pages-v33";
 const ASSETS = [
   "./",
   "./index.html",
